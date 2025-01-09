ea2bdd0f91edcb2c908613f8163ae6b086674598
karim1311 <valenzuelakarim03@gmail.com>
2025-01-08 21:03:23 -0700

    Excercise 1 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // ask for firstname with console write, this just prints this text
+        Console.Write("Please enter your first name: ");
+
+        // we save the variable here reading our input and saving into a string variable
+        string fname = Console.ReadLine();
+
+        // we do the same for last name
+        Console.Write("Please enter your last name: ");
+        string lname = Console.ReadLine();
+
+        // we print variables inside a string with dollar sign and curly braces
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
